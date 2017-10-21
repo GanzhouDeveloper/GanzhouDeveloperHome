9cae132a7d316b77daccb4aaffb91721d5543743
Rod <supgeek.rod@gmail.com>
2017-10-21 10:16:57 +0800

    Official WebSite

---
+++ resources/views/home/index.blade.php
@@ -0,0 +1,341 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>赣州开发者官方网站</title>
+    <!-- Meta -->
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="">
+    <meta name="author" content="">    
+    <link rel="shortcut icon" href="favicon.ico">  
+    <link href='https://fonts.googleapis.com/css?family=Raleway:400,500,600,700,800,900' rel='stylesheet' type='text/css'>
+    <link href='https://fonts.googleapis.com/css?family=Source+Sans+Pro:400,600,700,400italic,600italic,700italic' rel='stylesheet' type='text/css'>
+    <!-- Global CSS -->
+    <link rel="stylesheet" href="assets/plugins/bootstrap/css/bootstrap.min.css">   
+    <!-- Plugins CSS -->   
+    <link rel="stylesheet" href="assets/plugins/font-awesome/css/font-awesome.min.css">
+    <link rel="stylesheet" href="assets/plugins/vegas/dist/vegas.min.css">
+    
+    <!-- Theme CSS -->
+    <link id="theme-style" rel="stylesheet" href="assets/css/styles.css">
+
+</head> 
+
+<body data-spy="scroll">     
+    <!-- ******HEADER****** --> 
+    <header id="header" class="header fixed-top">  
+        <div class="container ">       
+            <nav class="main-nav navbar navbar-expand-md" role="navigation">
+                <button class="navbar-toggler navbar-dark " type="button" data-toggle="collapse" data-target="#navbar-collapse" aria-controls="navbar-collapse" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="toggle-title">Menu</span>
+                    <span class="navbar-toggler-icon"></span>
+                </button>  
+                <div id="navbar-collapse" class="navbar-collapse collapse text-center justify-content-center">
+                    <ul class="nav navbar-nav">
+                        <li class="nav-item"><a class="active nav-link scrollto" href="#promo">首页</a></li>
+                        <li class="nav-item"><a class="nav-link scrollto" href="#events">近期活动</a></li>
+                        <li class="nav-item"><a class="nav-link scrollto" href="#about">加入我们</a></li>
+                        <li class="nav-item"><a class="nav-link scrollto" href="#links">支持与贡献</a></li>
+                    </ul><!--//nav-->
+                </div><!--//navabr-collapse-->
+            </nav><!--//main-nav-->
+        </div><!--//container-->
+    </header><!--//header-->
+    
+    <!-- ******Promo Section****** -->
+    <section id="promo" class="promo-section section">
+        <div class="container text-center promo-content">
+            <div class="upper-wrapper">
+                <h1 class="">&nbsp;</h1>
+                <h2 class="headline">赣州开发者</h2>
+                <div class="tagline">Information technology drives Ganzhou 's economic development</div>
+            </div>
+        </div><!--//container-->    
+        
+        <div class="updates-block">
+            
+            <div class="container updates-block-inner">
+                <div id="carousel-updates" class="carousel slide" data-ride="carousel" data-interval="6000">
+                    <!-- Wrapper for slides -->
+                    <div class="carousel-inner" role="listbox">
+                        <div class="carousel-item active">
+                            <div class="carousel-content no-media-holder">
+                                <h3 class="title">赣州开发者，用信息技术驱动赣州经济发展</h3>
+                                <div class="desc">
+                                    <p class="intro">
+                                        如今，信息技术影响着每一个人的生活。在世界上市值排名前 10 的公司有 6 家是互联网或软件企业 <br>
+                                        在赣州有一群信息技术从业者
+                                    </p><!--//intro-->
+                                </div><!--//desc-->
+                            </div><!--//content-container-->
+                        </div><!--//item-->
+                    </div><!--//carousel-inner-->
+
+                    <!-- Controls -->
+                    <a class="carousel-control-prev" href="#carousel-updates" role="button" data-slide="prev">
+                        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                        <span class="sr-only">Previous</span>
+                    </a>
+                    <a class="carousel-control-next" href="#carousel-updates" role="button" data-slide="next">
+                        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                        <span class="sr-only">Next</span>
+                    </a>
+                </div><!--//carousel-updates-->
+                
+                
+            </div><!--//container-->
+        </div><!--//updates-block-->
+        
+    </section><!--//promo-section-->
+    
+    <section id="events" class="gigs-section section">
+        <div class="container">
+            <h2 class="section-title text-center">近期活动</h2>
+            <div class="gigs-container">
+                <div class="item">
+                    <div class="date-label">
+                        <div class="number">29</div>
+                        <div class="month">10月</div>
+                    </div><!--//date-label-->
+                    <div class="gig-info">
+                        <div class="info-content">
+                            <h3 class="gig-title">赣州开发者第三期线下活动</h3>
+                            <div class="meta">
+                                <ul class="meta-list list-inline">
+                                    <li class="list-inline-item location">
+                                        <i class="fa fa-map-marker"></i><a href="#">章贡区, 赣坊青创公社</a>
+                                    </li>
+                                    <li class="list-inline-item time">
+                                        <i class="fa fa-clock-o"></i>9:00pm, 星期日
+                                    </li>
+                                </ul>
+                            </div><!--//meta-->
+                        </div><!--//info-content-->
+                        <div class="gig-actions">
+                            <a class="btn btn-xs btn-ghost-secondary" data-toggle="collapse" href="#info-extra-1" aria-expanded="false" aria-controls="info-extra-1" >更多信息</a>
+                             <a class="btn btn-xs btn-secondary" href="#">报名</a>
+                        </div><!--//gig-actions-->
+                        <div id="info-extra-1" class="collapse info-extra">
+                            这是赣州开发者第三期线下交流活动，继第二期活动时隔四个月。这一次我们投入更多的精力去筹备活动，相信这会是一次更精彩的活动 ~
+                        </div>
+                    </div><!--//info-extra-->
+                </div><!--//item-->
+            </div><!--//gigs-container-->
+        </div><!--//container-->
+    </section><!--//gigs-section-->
+
+    <section id="about" class="about-section section text-center" style="padding-top:0;">
+        <div class="container">
+            <h2 class="section-title">加入我们</h2>
+            <div class="section-intro ml-auto mr-auto">
+                如果你是一名互联网从业者，不管你是软件工程师还是硬件工程师，亦或者是产品经理、项目经理、设计师、运维工程师 ...
+                都欢迎你加入我们这个积极的小组织中来。一起交流和分享行业知识，碰撞灵感，协同创作 ~
+            </div>
+            <div class="members-block">
+                <div class="row">
+                    <div class="item col-6 col-lg-3">
+                        <div class="item-inner">
+                            <div class="member-profile">
+                                <img class="img-fluid" src="assets/images/members/member-1.png" alt="">
+                            </div><!--//member-profile-->
+                            <div class="member-label">Vocals</div>
+                            <h3 class="member-name">
+                                Daniel Contreras
+                            </h3>
+                            <div class="member-desc">
+                                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ipsum neque, ornare vitae auctor vitae, blandit sit amet metus. Nulla vestibulum sem odio, eget tempor nisl mattis et.
+                            </div>
+                        </div><!--//item-inner-->
+                    </div><!--//item-->
+                    <div class="item col-6 col-lg-3">
+                        <div class="item-inner">
+                            <div class="member-profile">
+                                <img class="img-fluid" src="assets/images/members/member-2.png" alt=""> 
+                            </div><!--//member-profile-->
+                            <div class="member-label">Guitar</div>
+                            <h3 class="member-name">
+                                Tim Wallace
+                            </h3>
+                            <div class="member-desc">
+                                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ipsum neque, ornare vitae auctor vitae, blandit sit amet metus. Nulla vestibulum sem odio, eget tempor nisl mattis et.
+                            </div>
+                        </div><!--//item-inner-->
+                    </div><!--//item-->
+                    <div class="clearfix visible-xs"></div>
+                    <div class="item col-6 col-lg-3">
+                        <div class="item-inner">
+                            <div class="member-profile">
+                                <img class="img-fluid" src="assets/images/members/member-3.png" alt="">
+                                
+                            </div><!--//member-profile-->
+                            <div class="member-label">Bass</div>
+                            <h3 class="member-name">
+                                Andrew Bryant
+                            </h3>
+                            <div class="member-desc">
+                                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ipsum neque, ornare vitae auctor vitae, blandit sit amet metus. Nulla vestibulum sem odio, eget tempor nisl mattis et.
+                            </div>
+                        </div><!--//item-inner-->
+                    </div><!--//item-->
+                    <div class="item col-6 col-lg-3">
+                        <div class="item-inner">
+                            <div class="member-profile">
+                                <img class="img-fluid" src="assets/images/members/member-4.png" alt="">
+                                
+                            </div><!--//member-profile-->
+                            <div class="member-label">Drums</div>
+                            <h3 class="member-name">
+                                Ray Anderson
+                            </h3>
+                            <div class="member-desc">
+                                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ipsum neque, ornare vitae auctor vitae, blandit sit amet metus. Nulla vestibulum sem odio, eget tempor nisl mattis et.
+                            </div>
+                        </div><!--//item-inner-->
+                    </div><!--//item-->
+                </div><!--//row-->
+            </div><!--//members-block-->
+        </div><!--//container-->
+    </section><!--//about-section-->
+
+    <section id="photos" class="merch-section section text-center" style="display:none;">
+        <div class="container">
+            <h2 class="section-title">精彩时刻</h2>
+            <div class="row">
+                <div class="item col-12 col-md-6">
+                    <div class="item-inner">
+                        <img class="img-fluid" src="assets/images/merch/merch-1.jpg" alt="">
+                        <a class="mask" href="#">
+                            <span class="mask-inner">
+                                <span class="item-title">Band T-Shirt $16</span>
+                                <span class="desc">Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor aenean massa.</span>
+                                <span class="btn btn-primary btn-cta">Buy Now</span>
+                            </span>
+                        </a>
+                    </div><!--//item-inner-->
+                </div>
+                <div class="item col-12 col-md-6">
+                    <div class="item-inner">
+                        <img class="img-fluid" src="assets/images/merch/merch-2.jpg" alt="">
+                        <a class="mask" href="#">
+                            <span class="mask-inner">
+                                <span class="item-title">Band T-Shirt $16</span>
+                                <span class="desc">Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor aenean massa.</span>
+                                <span class="btn btn-primary btn-cta">Buy Now</span>
+                            </span>
+                        </a>
+                    </div><!--//item-inner-->
+                </div>
+                <div class="item col-12 col-md-6">
+                    <div class="item-inner">
+                        <img class="img-fluid" src="assets/images/merch/merch-3.jpg" alt="">
+                        <a class="mask" href="#">
+                            <span class="mask-inner">
+                                <span class="item-title">Band Hoody $16</span>
+                                <span class="desc">Lorem ipsum dolor sit amet, consectetuer adipiscing elit.</span>
+                                <span class="btn btn-primary btn-cta">Buy Now</span>
+                            </span>
+                        </a>
+                    </div>
+                </div>
+                <div class="item col-12 col-md-6">
+                    <div class="item-inner">
+                         <img class="img-fluid" src="assets/images/merch/merch-4.jpg" alt="">
+                         <a class="mask" href="#">
+                            <span class="mask-inner">
+                                <span class="item-title">Band Tote Bag $8</span>
+                                <span class="desc">Lorem ipsum dolor sit amet, consectetuer adipiscing elit.</span>
+                                <span class="btn btn-primary btn-cta">Buy Now</span>
+                            </span>
+                        </a>
+                    </div>
+                </div>
+            </div><!--//row-->
+            
+            <div class="merch-action">
+                <a class="btn btn-ghost-primary" href="#">View All Items</a>
+            </div>
+            
+        </div><!--//container-->
+        
+    </section><!--//merch-section-->
+    
+    <div id="links" class="contact-section section text-center" style="padding-bottom:1px;">
+        <div class="container">
+            <h2 class="section-title">支持与贡献</h2>
+            <div class="section-intro center-block">
+                感谢以下企业和个人对本组织的支持与贡献
+            </div>
+            <div class="channels-block">
+                <ul class="channels-list list-inline">
+                    <li class="list-inline-item"><a href="#" class="h5">进托邦科技</a></li>
+                    <li class="list-inline-item"><a href="#" class="h5">尚行科技</a></li>
+                    <!--
+                    <li class="list-inline-item"><a href="#"><img class="icon" src="assets/images/itunes.svg" alt=""></a></li>
+                    -->
+                </ul>
+            </div><!--//channels-block-->
+            <!--
+            <div class="social-media-block" style="display:none;">
+                <ul class="list-inline social-media-list">
+                    <li class="list-inline-item"><a href="#"><i class="fa fa-youtube"></i></a></li>
+                    <li class="list-inline-item"><a href="#"><i class="fa fa-soundcloud"></i></a></li>
+                    <li class="list-inline-item"><a href="#"><i class="fa fa-spotify"></i></a></li>
+                    <li class="list-inline-item"><a href="#"><i class="fa fa-facebook"></i></a></li>
+                    <li class="list-inline-item"><a href="#"><i class="fa fa-instagram"></i></a></li>
+                    <li class="list-inline-item"><a href="#"><i class="fa fa-twitter"></i></a></li>
+                </ul>
+            </div><!--//social-media-block-->
+        </div><!--//containter-->
+    </div>
+    
+    <!-- ******FOOTER****** --> 
+    <footer class="footer">
+        <div class="container">
+            <div class="footer-content text-center">
+                <div class="copyright">
+                    @2017 <strong>Ganzhou Developer</strong>
+                    <br>
+                    Powered by ProtobiaTech
+                </div>
+            </div><!--//footer-content--> 
+        </div><!--//container-->
+    </footer><!--//footer-->
+    
+
+    <!-- *****CONFIGURE STYLE (REMOVE ON YOUR PRODUCTION SITE)****** -->
+    <div id="config-panel" class="config-panel hidden-xs">
+        <div class="panel-inner">
+            <a id="config-trigger" class="config-trigger config-panel-hide" href="#"><i class="fa fa-sliders"></i></a>
+            <h5 class="panel-title">Choose Colour</h5>
+            <ul id="color-options" class="list-unstyled list-inline">
+                <li class="list-inline-item theme-1 active"><a data-style="assets/css/styles.css" href="#"></a></li>
+                <li class="list-inline-item theme-2"><a data-style="assets/css/styles-2.css" href="#"></a></li>
+                <li class="list-inline-item theme-3"><a data-style="assets/css/styles-3.css" href="#"></a></li>
+                <li class="list-inline-item theme-4"><a data-style="assets/css/styles-4.css" href="#"></a></li>                   
+                <li class="list-inline-item theme-5"><a data-style="assets/css/styles-5.css" href="#"></a></li>                     
+                <li class="list-inline-item theme-6"><a data-style="assets/css/styles-6.css" href="#"></a></li>
+                <li class="list-inline-item theme-7"><a data-style="assets/css/styles-7.css" href="#"></a></li>
+                <li class="list-inline-item theme-8"><a data-style="assets/css/styles-8.css" href="#"></a></li>                    
+                <li class="list-inline-item theme-9"><a data-style="assets/css/styles-9.css" href="#"></a></li>
+                <li class="list-inline-item theme-10"><a data-style="assets/css/styles-10.css" href="#"></a></li>
+            </ul>
+            <a id="config-close" class="close" href="#"><i class="fa fa-times-circle"></i></a>
+        </div><!--//panel-inner-->
+    </div><!--//configure-panel-->
+
+ 
+    <!-- Main Javascript -->          
+    <script type="text/javascript" src="assets/plugins/jquery-3.2.1.min.js"></script>
+    <script type="text/javascript" src="assets/plugins/popper.min.js"></script> 
+    <script type="text/javascript" src="assets/plugins/bootstrap/js/bootstrap.min.js"></script>
+    <script type="text/javascript" src="assets/plugins/jquery-scrollTo/jquery.scrollTo.min.js"></script>  
+    <script type="text/javascript" src="assets/plugins/back-to-top.js"></script>
+    <script type="text/javascript" src="assets/plugins/vegas/dist/vegas.min.js"></script>
+    <script type="text/javascript" src="assets/js/main.js"></script> 
+        
+    <!-- Style Switcher (REMOVE ON YOUR PRODUCTION SITE) -->
+    <script src="assets/js/demo/style-switcher.js"></script>
+</body>
+</html> 
+
